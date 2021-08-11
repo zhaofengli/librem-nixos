b53062f5b2a44d18fa9b1f3e3c373ef734c177a4
Zhaofeng Li <hello@zhaofeng.li>
2021-08-11 15:57:48 -0700

    Remove patched upower
    No longer needed with Phosh 0.13.0.

--- default.nix
+++ default.nix
@@ -2,6 +2,5 @@ self: super:
 {
   librem = {
     gtk3 = super.callPackage ./pkgs/gtk3 {};
-    upower = super.callPackage ./pkgs/upower {};
   };
 }
--- pkgs/upower/README.md
+++
@@ -1,3 +0,0 @@
-# UPower
-
-UPower with [patch](https://gitlab.freedesktop.org/upower/upower/-/merge_requests/50) to add torch support.
